1fde80347e5bc166cb30db46eceb762d38a55278
claitonneri <claitondidone@gmail.com>
2018-08-16 11:19:38 -0300

    Layout modificado

--- app/src/main/java/br/com/claitonneri/requisicaohttp/MainActivity.java
+++ app/src/main/java/br/com/claitonneri/requisicaohttp/MainActivity.java
@@ -3,8 +3,13 @@ package br.com.claitonneri.requisicaohttp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+        private Button btnRecuperar;
+        private TextView txtResultado;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
